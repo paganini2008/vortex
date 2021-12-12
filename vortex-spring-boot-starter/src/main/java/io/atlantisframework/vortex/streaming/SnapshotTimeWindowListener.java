0a0c0c257757a0140b6e5ac289c0d67edf1d36d1
Fred Feng <paganini2008@sina.com>
2021-12-12 16:37:17 +0800

    commit

--- vortex-spring-boot-starter/src/main/java/io/atlantisframework/vortex/streaming/SnapshotTimeWindowListener.java
+++ vortex-spring-boot-starter/src/main/java/io/atlantisframework/vortex/streaming/SnapshotTimeWindowListener.java
@@ -1,8 +1,24 @@
+/**
+* Copyright 2017-2021 Fred Feng (paganini.fy@gmail.com)
+
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package io.atlantisframework.vortex.streaming;
 
 import java.time.Instant;
 import java.util.List;
 
+import com.github.paganini2008.devtools.collection.CollectionUtils;
 import com.github.paganini2008.devtools.time.TimeWindowListener;
 
 import lombok.extern.slf4j.Slf4j;
@@ -20,7 +36,9 @@ public class SnapshotTimeWindowListener<T> implements TimeWindowListener<T> {
 	@Override
 	public void saveCheckPoint(Instant time, List<T> values) {
 		if (log.isInfoEnabled()) {
-			log.info("[{}] show info: {}", time.toString(), values.stream().findAny().get());
+			if (CollectionUtils.isNotEmpty(values)) {
+				log.info("[{}] size: {}, info: {}", time.toString(), values.size(), values.stream().findAny().get());
+			}
 		}
 	}
 
